e83c451299958727bcd816cfb32525d54c649df2
StdBots <stdbots@gmail.com>
2026-01-28 13:21:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")
+SUPPORT_LINK = os.getenv("SUPPORT_LINK")
